61d74380b956461a0b729bec83fe40706a93c4ec
Ambal-Ced <147784891+Ambal-Ced@users.noreply.github.com>
2024-02-06 09:21:31 +0000

    place holder
    place holder

---
+++ .ccls-cache/@home@runner@ADBMS-1/connection.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+/*place holder*/
+using namespace std;
+
+int main() {
+  int a = 5;
+  int b = 3;
+  int sum = a + b;
+  int difference = a - b;
+  int product = a * b;
+  float quotient = (float)a / b;
+
+  cout << "Sum: " << sum << endl;
+  cout << "Difference: " << difference << endl;
+  cout << "Product: " << product << endl;
+  cout << "Quotient: " << quotient << endl;
+
+  return 0;
+}
